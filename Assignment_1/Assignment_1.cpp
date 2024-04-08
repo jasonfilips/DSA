7974eedd3faef0b2df2b8d3f02d51d3bc79844b8
jasonfilips <jasonfilipss@yahoo.com>
2024-04-08 20:28:28 +0300

    Created a function to analyze the completion speed of an algorithm

--- Assignment_1/Assignment_1.cpp
+++ Assignment_1/Assignment_1.cpp
@@ -1,6 +1,24 @@
 #include "Serialization.cpp"
 #include <chrono>
 
+void algorithmExample() {
+    for (int i = 0; i < 100000000; i++) {
+        int result = i * i;
+    }
+}
+
+void analyzeSpeed() {
+    std::cout << "Testing the speed of the algorithm" << std::endl;
+    auto start = std::chrono::steady_clock::now();
+    //run the algorithm 10 times
+    for (int i = 0; i < 10; i++) {
+        algorithmExample();
+    }
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "Time to complete the algorithm " << duration.count() << " seconds" << std::endl;
+}
+
 int main()
 {
     //Example
@@ -9,6 +27,8 @@ int main()
         {"Legend of the Galactic Heroes: Dawn", Genre::SpaceOpera, 292},
         {"House of Leaves", Genre::Mystery, 736}
     };
+
+    analyzeSpeed();
 
     //Serialize data
     serializeData(books, "books.csv");
