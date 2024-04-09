38e682a05f75edd522073f1b855d396670fc5e75
jasonfilips <jasonfilipss@yahoo.com>
2024-04-10 02:13:19 +0300

    Added a search function to search for a book with a specific title and return the node which contains it and the previous one

--- Assignment_2/LinkedList.cpp
+++ Assignment_2/LinkedList.cpp
@@ -1,4 +1,5 @@
 #include "../Assignment_1/Serialization.h"
+#include <vector>
 
 struct ListNode {
     BookData book;
@@ -15,7 +16,7 @@ private:
     ListNode* insertNode(ListNode* node, const BookData& book);
     ListNode* insertAfterNode(LinkedList* list, ListNode* node, const BookData& book);
     void removeAfter(LinkedList* list, ListNode* node);
-    bool searchNodeByTitle(ListNode* node, const std::string& searchItem) const;
+    std::vector<ListNode*> searchNodeByTitle(ListNode* node, const std::string& searchItem, std::vector<ListNode*>& foundNodes) const;
     void createList(const std::vector<BookData>& books);
     void deleteList(ListNode* node);
     void assertNoCycles(LinkedList* list, ListNode* node);
@@ -27,7 +28,7 @@ public:
     void insert(const BookData& book);
     ListNode* insertAfter(LinkedList* list, ListNode* node, const BookData& book);
     void remove(LinkedList* list, ListNode* node);
-    bool search(const std::string& searchItem) const;
+    std::vector<ListNode*> search(const std::string& searchItem, std::vector<ListNode*>& foundNodes) const;
     void create(const std::vector<BookData>& books);
 };
 
@@ -41,6 +42,44 @@ void LinkedList::deleteList(ListNode* node) {
     if (node) {
         deleteList(node->nextNode);
         delete node;
+    }
+}
+
+//Public function to insert a new node into the list
+void LinkedList::insert(const BookData& book) {
+    rootNode = insertNode(rootNode, book);
+}
+
+//Private recursive function to insert a node into the tree
+ListNode* LinkedList::insertNode(ListNode* node, const BookData& book) {
+    if (node == nullptr) {
+        return new ListNode(book);
+    }
+    else {
+        node->nextNode = insertNode(node->nextNode, book);
+    }
+
+    return node;
+}
+
+//Public function to search for a node by the title and returns the node and the one before it
+std::vector<ListNode*> LinkedList::search(const std::string& searchItem, std::vector<ListNode*>& foundNodes) const {
+    return searchNodeByTitle(rootNode, searchItem, foundNodes);
+}
+
+//Private function to search for a node by the title and returns a vector which contains the node and the one before it
+std::vector<ListNode*> LinkedList::searchNodeByTitle(ListNode* node, const std::string& searchItem, std::vector<ListNode*>& foundNodes) const {
+
+    if (node == nullptr) {
+        return foundNodes;
+    }
+
+    if (node->book.title == searchItem) {
+        foundNodes.push_back(node);
+    }
+    else {
+        foundNodes[0] = node;
+        return searchNodeByTitle(node->nextNode, searchItem, foundNodes);
     }
 }
 
